d6b3ccba996b448cd4b2f4d776700d6d2ae38888
marix-shiv <sukumar.ae5@gmail.com>
2024-03-29 18:46:04 +0530

    initial commit

--- azalfrontend/src/App.js
+++ azalfrontend/src/App.js
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Header from "./components/common/Header";
 import { Outlet } from "react-router-dom";
+import Footer from "./components/common/Footer";
 
 const App = () => {
+  useEffect(()=>{
+    const data = fetch(process.env.REACT_APP_USERS_API).then(res => res.json()).then(res1 => console.log(res1))
+  },[])
   return (
     <>
       <Header />
       <Outlet />
+      <Footer />
     </>
   );
 };
