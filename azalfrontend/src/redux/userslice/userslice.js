d9c5bfdadd482785835857e5b8e8248fb8c4ac22
marix-shiv <sukumar.ae5@gmail.com>
2024-04-11 14:34:51 +0530

    initial commit

--- azalfrontend/src/redux/userslice/userslice.js
+++ azalfrontend/src/redux/userslice/userslice.js
@@ -1,17 +1,38 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  //Write initial state here
+  //initial state here
+  id: "",
+  email: "",
+  username: "",
+  name: "",
+  image: "",
 };
 
 export const userslice = createSlice({
   name: "users",
   initialState,
   reducers: {
-    //Write redux code here
+    //Login and Logout redux code here
+    loginRedux: (state, action) => {
+      state.id = action.payload.id;
+      state.username = action.payload.username;
+      state.email = action.payload.email;
+      state.name = action.payload.name;
+      state.image = action.payload.image;
+    },
+    logoutRedux: (state, action) => {
+      // console.log(action.payload);
+      state.id = "";
+      state.username = "";
+      state.email = "";
+      state.fistname = "";
+      state.lastname = "";
+      state.image = "";
+    },
   },
 });
 
 //uncomment this line to use userslice
-// export const {} = userslice.actions; 
+export const { loginRedux, logoutRedux } = userslice.actions;
 export default userslice.reducer;
