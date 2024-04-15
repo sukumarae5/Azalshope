e55f6bc7dc6e06bded942c76d01eb54971ad6c7d
marix-shiv <sukumar.ae5@gmail.com>
2024-04-15 15:02:57 +0530

    updated cart button

--- azalfrontend/src/screens/Homescreen.js
+++ azalfrontend/src/screens/Homescreen.js
@@ -16,9 +16,9 @@ const Homescreen = ({
 }) => {
   const data = useSelector((state) => state.products.allProducts);
   const dispatch = useDispatch();
-  
+
   const userData = useSelector((state) => state.users);
-  if(userData !== ''){
+  if (userData !== "") {
     console.log(userData);
   }
 
@@ -73,7 +73,7 @@ const Homescreen = ({
 
   useEffect(() => {
     filterProducts();
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedItem]);
   return (
     <>
@@ -99,8 +99,18 @@ const Homescreen = ({
         }}
       >
         {filterProduct.map((item, idx) => (
-          <div key={`items-${idx}`} className="item" style={{boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px"}}>
-            <img classname="image" id="img1" alt="img" src={item.images} style={{  height: "200px",width: "250px"}}/>
+          <div
+            key={`items-${idx}`}
+            className="item"
+            style={{ boxShadow: "rgba(0, 0, 0, 0.24) 0px 3px 8px" }}
+          >
+            <img
+              classname="image"
+              id="img1"
+              alt="img"
+              src={item.images}
+              style={{ height: "200px", width: "250px" }}
+            />
             <p>{item.title}</p>
             <h4>{item.brand}</h4>
 
@@ -111,6 +121,7 @@ const Homescreen = ({
             <p className="text-danger">{item.discountPercentage}%OFF</p>
             <p>{getRating(item.rating)}</p>
             <p className="category">{item.category}</p>
+            <div className="d-flex align-items-center justify-content-center">
             <button
               onClick={() => {
                 dispatch(
@@ -126,10 +137,22 @@ const Homescreen = ({
                   })
                 );
               }}
-              className="btn btn-primary"
-            >
-              Add to Cart
+              className="CartBtn"
+              >
+              <span class="IconContainer">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  height="1em"
+                  viewBox="0 0 576 512"
+                  fill="rgb(17, 17, 17)"
+                  class="cart"
+                  >
+                  <path d="M0 24C0 10.7 10.7 0 24 0H69.5c22 0 41.5 12.8 50.6 32h411c26.3 0 45.5 25 38.6 50.4l-41 152.3c-8.5 31.4-37 53.3-69.5 53.3H170.7l5.4 28.5c2.2 11.3 12.1 19.5 23.6 19.5H488c13.3 0 24 10.7 24 24s-10.7 24-24 24H199.7c-34.6 0-64.3-24.6-70.7-58.5L77.4 54.5c-.7-3.8-4-6.5-7.9-6.5H24C10.7 48 0 37.3 0 24zM128 464a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm336-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z"></path>
+                </svg>
+              </span>
+              <p class="text">Add to Cart</p>
             </button>
+          </div>
           </div>
         ))}
       </div>
